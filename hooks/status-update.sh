298ba7f7f414f295d2445291455cf81669f25b95
Christoph Kröppl <ch.kroeppl@gmx.at>
2026-03-25 11:33:19 +0100

    fix(hooks): ensure scripts exit with 0 outside of git repositories

--- hooks/status-update.sh
+++ hooks/status-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jq -r ".llm_request.model + \"|\" + ((.llm_response.usageMetadata.totalTokenCount // 0) | tostring)" > /tmp/.gemini_status_current
+jq -r ".llm_request.model + \"|\" + ((.llm_response.usageMetadata.totalTokenCount // 0) | tostring)" > /tmp/.gemini_status_current || true
